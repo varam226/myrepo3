b7e593e09a3b254399b3174165c2865a8b2a63a3
varam226 <varalakshmidamanaboyina79@gmail.com>
2025-11-08 18:58:37 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,12 @@
 System.out.println("Hello World!")
 new code by developer 1 without pulling the data;
 adding data to know more about pull
+
+
+public class Main {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+}
+  
